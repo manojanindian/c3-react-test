32b895e70933d2d0c11a213b0b20024defd58d7b
Manoj Kumar 14 <mkumar79@sapient.com>
2018-06-27 13:14:34 +0530

    manoj-kumar: updated PearsonUsers component and test cases

--- src/PearsonUsers.js
+++ src/PearsonUsers.js
@@ -1,42 +1,120 @@
 import React, { Component } from "react";
 
+/**
+ * UserProfile component will render following elements
+ * User's image, name and delete button
+ */
+
+const UserProfile = ({first_name, last_name, avatar, id, onClickDeleteUser}) => {
+	return (
+		<div className="user-profile">
+			<img
+				alt={`${first_name} ${last_name}`}
+				className="user-avatar"
+				src={avatar} 
+			/>
+			<h3
+				className="user-name"
+			>
+				{`${first_name} ${last_name}`}
+			</h3>
+			<button
+				className="user-delete-btn"
+				type="button"
+				onClick={() => onClickDeleteUser(id)}
+			>
+				Delete
+			</button>
+		</div>
+	)
+}
+
+
 export class PearsonUsers extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      users: [
-        {
-          id: 4,
-          first_name: "Eve",
-          last_name: "Holt",
-          avatar:
-            "https://s3.amazonaws.com/uifaces/faces/twitter/marcoramires/128.jpg"
-        },
-        {
-          id: 5,
-          first_name: "Charles",
-          last_name: "Morris",
-          avatar:
-            "https://s3.amazonaws.com/uifaces/faces/twitter/stephenmoon/128.jpg"
-        },
-        {
-          id: 6,
-          first_name: "Tracey",
-          last_name: "Ramos",
-          avatar:
-            "https://s3.amazonaws.com/uifaces/faces/twitter/bigmancho/128.jpg"
-        }
-      ]
-    };
-  }
+		this.state = {
+			users: [
+			{
+				id: 4,
+				first_name: "Eve",
+				last_name: "Holt",
+				avatar:
+				"https://s3.amazonaws.com/uifaces/faces/twitter/marcoramires/128.jpg"
+			},
+			{
+				id: 5,
+				first_name: "Charles",
+				last_name: "Morris",
+				avatar:
+				"https://s3.amazonaws.com/uifaces/faces/twitter/stephenmoon/128.jpg"
+			},
+			{
+				id: 6,
+				first_name: "Tracey",
+				last_name: "Ramos",
+				avatar:
+				"https://s3.amazonaws.com/uifaces/faces/twitter/bigmancho/128.jpg"
+			}
+			]
+		};
+		
+		this.onClickDeleteUser = this.onClickDeleteUser.bind(this);
+	}
 
-  render() {
-    return (
-      <div className="pearon-users">
-        <h1>Pearson User Management</h1>
-        {/* Render users here */}
-      </div>
-    );
+  componentDidMount() {
+		fetch('https://reqres.in/api/users?page=1&per_page=10')
+		.then(data => data.json())
+		.then((data) => {
+			const newUserList = this.removeDuplicateEntries(data.data);
+			this.setState({
+				users: newUserList
+			});
+		}); 
   }
+	  
+	/**
+	 * removeDuplicateEntries() function is responsible to remove duplicate entries from fetch response
+	 * function will remove duplicate entries and will concate oldUsers data with newUsers data
+	 */
+
+	removeDuplicateEntries(newUsers) {
+		const oldUsersId = [...(new Set(this.state.users.map(({id}) => id)))];
+		const newUsersId = new Set(newUsers.map(({id}) => id));
+
+		// duplicate users list
+		const duplicateUsers = new Set(oldUsersId.filter(userId => newUsersId.has(userId)));
+
+		return [...this.state.users, ...newUsers.filter( user => !duplicateUsers.has(user.id))];
+	}
+
+	/**
+	 * onClickDeleteUser() function will remove respected user from component state
+	 */
+
+	onClickDeleteUser(userId) {
+		const users = this.state.users.filter(function(user) { 
+			return user.id !== userId
+		})
+
+		this.setState({
+			users
+		});
+	}
+
+  	render() {
+		const usersProfile = this.state.users.map( (user, index) => 
+			<UserProfile key={index} {...user} onClickDeleteUser={this.onClickDeleteUser} />
+		);
+
+		return (
+			<div className="pearson-users">
+				<h1 className="title">Pearson User Management</h1>
+				<div className="user-list">
+					{usersProfile}
+				</div>
+			</div>
+		);
+	}
 }
--- src/PearsonUsers.spec.js
+++ src/PearsonUsers.spec.js
@@ -3,14 +3,95 @@ import { shallow } from "enzyme";
 import { PearsonUsers } from "./PearsonUsers";
 
 describe("PearsonUsers", () => {
-  let component;
+	let component;
+	/**
+	 * Mock global fetch funciton
+	 * fetch will return response having 3 users
+	 * In 3 users one is duplicate which id is 4
+	 */
+	
+	beforeEach(() => {
+		fetch = jest.fn().mockImplementationOnce(() => {
+			return new Promise((resolve, reject) => {
+				resolve({
+					json: () => {
+						return {data: [
+							{
+								id: 1,
+								first_name: "Eve",
+								last_name: "Holt",
+								avatar:
+									"https://s3.amazonaws.com/uifaces/faces/twitter/marcoramires/128.jpg"
+							},
+							{
+								id: 4,
+								first_name: "Charles",
+								last_name: "Morris",
+								avatar:
+									"https://s3.amazonaws.com/uifaces/faces/twitter/stephenmoon/128.jpg"
+							},
+							{
+								id: 8,
+								first_name: "Charles",
+								last_name: "Morris",
+								avatar:
+									"https://s3.amazonaws.com/uifaces/faces/twitter/stephenmoon/128.jpg"
+							}
+						]};
+					},
+				});
+			});
+		});
+	
+		component = shallow(<PearsonUsers />);
+		
+	});
 
-  beforeEach(() => {
-    component = shallow(<PearsonUsers />);
-  });
+	it("component has title", () => {
+		const h1 = component.find("h1.title");
+    	expect(h1.text()).toEqual("Pearson User Management");
+	});
 
-  it("renders a h1", () => {
-    const h1 = component.find("h1");
-    expect(h1.text()).toEqual("Pearson User Management");
-  });
+	/**
+	 * UserProfile componet must containe following theree elements
+	 * User avatar, user name and delete button
+	 */
+	  
+	it("User Profile has avatar, name and delete button", () => {
+		const userProfileComponent = component.find('UserProfile').first().dive();
+		expect(userProfileComponent.find('.user-avatar').length).toBe(1);
+		expect(userProfileComponent.find('.user-name').length).toBe(1);
+		expect(userProfileComponent.find('.user-delete-btn').length).toBe(1);
+	});
+
+	it("fetch should be called", () => {
+		expect(fetch.mock.calls.length).toBe(1);
+	});
+
+	it("fetch url should be 'https://reqres.in/api/users?page=1&per_page=10'", () => {
+		expect(fetch.mock.calls[0][0]).toBe('https://reqres.in/api/users?page=1&per_page=10');
+	});
+
+	/**
+	 * PearsonUsers component has already 3 users data in state [4, 5, 6]
+	 * Fetch is providing 3 more users data [1, 4, 8]
+	 * Total users should be 6 now but we have functionality to remove duplicate users [4]
+	 * hence Total users should be 5 instead 6
+	 */
+	
+	it("duplicate user should be deleted", () => {
+		expect(component.state().users.length).toBe(5);
+	});
+
+	/**
+	 * After fetch PersonUsers component has 5 users in state [4, 5, 6, 1, 8]
+	 * If user click on delete button user's data should be deleted from state
+	 * After delete total number of users will be 4 in state
+	 */
+	
+	it("delete user onclick of delete button", () => {
+		const userProfileComponent = component.find('UserProfile').first().dive();
+		userProfileComponent.find('.user-delete-btn').simulate('click');
+		expect(component.state().users.length).toBe(4);
+	});
 });
